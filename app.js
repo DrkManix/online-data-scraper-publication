0d590042d6b57d757132cb666fe92566df8b22c6
DrkManix <45601260+DrkManix@users.noreply.github.com>
2019-12-09 10:08:09 +0100

    update individuals

--- app.js
+++ app.js
@@ -26,22 +26,6 @@ app.get('/guardian', function(req, res) {
       // we can use the dependency 'cheerio' to traverse the DOM and use jQuery-like selectors and functions
       var $ = cheerio.load(html);
 
-      // let's create a javascript object to save our data in
-      var guardian_data = {
-        title: '',
-        img: '',
-        paragraph: ''
-      };
-
-      // all the content we are looking for are inside a div with the id 'content', let's filter so that the data we are working with is without unnecessary data
-      // $('.css-1wa7u5r').filter(function(){
-      //
-      //   // we can access the properties of our javascript object by writing the name of the object 'dot' and then the name of the property
-      //   guardian_data.title = $(this).find('h4').text();
-      //   guardian_data.img = $(this).find('img').attr('src');
-      //
-      // });
-
       var item_output = [];
       $('li[data-testid="search-bodega-result"]').filter(function(){
             // we can access the properties of our javascript object by writing the name of the object 'dot' and then the name of the property
@@ -52,112 +36,14 @@ app.get('/guardian', function(req, res) {
       // send the data we've stored in our object back to the browser
       res.send(item_output);
 
-      fs.writeFile('guardian_output.js', "var guardian_output = " + JSON.stringify(guardian_data), function(error){
+      fs.writeFile('guardian_output.js', "var guardian_output = " + JSON.stringify(  item_output ), function(error){
         console.log("File is written successfully!");
       });
     }
   });
 });
 
-app.get('/wikihow', function(){
-  var keywords = ["police", "brutality", "weapons"];
-  var urls = [];
 
-  for(word in keywords) {
-    http.get("https://www.wikihow.com/wikiHowTo?search=" + word, function(response) {
-      response.on('data', function(chunk){
-        var $ = cheerio.load(chunk);
-        $('a.result_link').each(function(index, element){
-          urls[index] = $(this).attr('href');
-        });
-      });
-    });
-  }
-
-  if(urls.length > 0) {
-    for(url in urls) {
-
-    }
-  }
-});
-
-
-// IMDB SCRAPER: access by going to 'localhost:2100/imdb'
-app.get('/imdb', function(req, res) {
-
-  var words = [];
-  var myWords = cheerio.load(fs.readFileSync('MyActivity.html'));
-
-  myWords.find('.content-cell a').each(function(i, elem){
-    if(myWords(this).text().includes("definition")) {
-
-      data = {
-        word: myWords(this).text(),
-        description: ''
-      };
-
-      words[i] = data;
-
-    }
-
-  });
-
-  for(word in words) {
-    http.get("https://www.merriam-webster.com/dictionary/" + words[i].word, function(res) {
-      var $ = cheerio.load(res);
-      $('#dictionary-entry-1').text();
-    });
-
-    }
-
-  // var url = "https://www.imdb.com/chart/top";
-
-
-
-  // let's make the http request to the url above using the 'request' dependency
-  request(url, function(error, response, html) {
-
-    // only execute if there's no error
-    if(!error){
-
-      // we can use the dependency 'cheerio' to traverse the DOM and use jQuery-like selectors and functions
-      var $ = cheerio.load(html);
-
-      // all the content we are looking for are inside a div with the class 'lister', let's filter so that the data we are working with is without unnecessary data
-      $('.postbody').filter(function(){
-
-        // there are a lot of 'tr' elements and for each of the 'tr' element we want to execute a function
-        $(this).find('p').each(function(index, element) {
-
-          for(var i = 0; i > names.length; i++) {
-
-            var result = $(this).text().split(names[i]);
-
-            if(result[0] === "PHOEBE") {
-              phoebe_data.push(result[1]);
-            } else if(result[0] === "JOEY") {
-              joey_data.push(result[1]);
-            } else if(result[0] === "RACHEL") {
-              rachel_data.push(result[1]);
-            }
-
-          }
-
-
-        });
-      });
-
-      // send the data we've stored in our array back to the browser
-      res.send(imdb_data);
-
-      // save the data we've stored in our object on our machine
-      fs.writeFile('./data/imdb_output.js', "var imdb_output = [" + imdb_data + "]" , function(error){
-        console.log("File is written successfully!");
-      });
-
-    }
-  });
-});
 
 // INSTAGRAM SCRAPER: access by going to 'localhost:2100/instagram'
 app.get('/instagram', function(req, res){
@@ -184,9 +70,9 @@ app.get('/instagram', function(req, res){
 
         download_file_curl(instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url);
 
-        fs.createWriteStream('./data/'+[i]+'.jpg', instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url, function(err){
-           console.log('File is written successfully!');
-         });
+        // fs.createWriteStream([i]+'.jpg', instagram_data.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url, function(err){
+        //    console.log('File is written successfully!');
+        //  });
       }
 
       // send the data we've stored in our array back to the browser
@@ -223,14 +109,6 @@ var download_file_curl = function(file_url) {
 };
 
 
-app.get('/twitch', function(req, res){
-  request('https://www.youtube.com/watch?v=28HABZJ358g', function(error, response, html){
-
-    res.send(html);
-
-  });
-});
-
 app.get('/guten', function(req, res) {
 
   var url = 'https://www.gutenberg.org/browse/scores/top';
@@ -246,13 +124,13 @@ app.get('/guten', function(req, res) {
         $(this).find('a').each(function(i, element) {
 
           // guten_data[i] = $(this).text();
-          guten_data[i] = "'" + $(this).attr('href') + "'";
+          guten_data[i] = $(this).attr('href');
 
         });
       });
 
       res.send(guten_data);
-      fs.writeFile('guten_output.js', "var guten_output = [" + guten_data + "]", function(error){
+      fs.writeFile('guten_output.js', "var guten_output = [" +  JSON.stringify( guten_data ) + "]", function(error){
         console.log("file is written successfully");
       });
 
